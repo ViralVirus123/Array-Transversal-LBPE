0071935d01b4c551e7b734031feb53bd46c87326
ViralVirus123 <alexander.hammond-garcia@patelhs.org>
2020-11-03 16:44:50 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,13 @@
+class Main {
+  public static void main(String[] args) {
+    int[] numberOfSnacks = {2, 5, 8, 11, 14, 17, 20, 23, 26, 29};
+    
+    for(int i = 9; i >= 0; i--){
+    if(numberOfSnacks[i] % 2 == 0);
+      System.out.println(numberOfSnacks[i]);
+    
+    
+    }
+
+  }
+}
